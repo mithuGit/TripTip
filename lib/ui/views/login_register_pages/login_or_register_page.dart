fe71afd5804d83daced2f400596e2a663fd52b31
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2023-11-15 16:42:23 +0100

    deleted lines

--- lib/ui/views/login_register_pages/login_or_register_page.dart
+++ lib/ui/views/login_register_pages/login_or_register_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:internet_praktikum/ui/views/login_register_pages/register_page.dart';
 
 import 'login_page.dart';
-
-
 class LoginOrRegisterPage extends StatefulWidget {
   const LoginOrRegisterPage({super.key});
 
