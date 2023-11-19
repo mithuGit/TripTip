80e233872a70e93c3b1ee3296395daffa23c352d
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2023-11-19 21:49:01 +0100

    add dev dependencies for firebase

--- test/test_username.dart
+++ test/test_username.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_praktikum/ui/widgets/usernamebagageCreateTrip.dart';
 void main() {
   // Define a test. The TestWidgets function also provides a WidgetTester
   // to work with. The WidgetTester allows building and interacting
   // with widgets in the test environment.
-  testWidgets('MyWidget has a title and message', (tester) async {
+  testWidgets('UsernameBagageCreateTrip has the correct title', (tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+    await tester.pumpWidget(const UsernameBagageCreateTrip());
 
     // Create the Finders.
     final titleFinder = find.text('T');
