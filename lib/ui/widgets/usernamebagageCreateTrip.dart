d4afdbdfff4f134fde00ab3b0916ea65454a67eb
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2023-11-23 12:23:16 +0100

    kleine formatierung

--- lib/ui/widgets/usernamebagageCreateTrip.dart
+++ lib/ui/widgets/usernamebagageCreateTrip.dart
@@ -7,79 +7,82 @@ class User {
   Image profileImage;
   User(this.prename, this.lastname, this.profileImage);
 }
+
 class UsernameBagageCreateTrip extends StatefulWidget {
   const UsernameBagageCreateTrip({super.key});
 
   @override
-  State<UsernameBagageCreateTrip> createState() => _UsernameBagageCreateTripState();
+  State<UsernameBagageCreateTrip> createState() =>
+      _UsernameBagageCreateTripState();
 }
+
 class _UsernameBagageCreateTripState extends State<UsernameBagageCreateTrip> {
-final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   Future<User> _getNames() async {
     Image pb = Image.asset('assets/Personavatar.png');
     if (_auth.currentUser != null) {
       String name = _auth.currentUser!.displayName!;
-      if(RegExp(r'.\ .').hasMatch(name)) {
+      if (RegExp(r'.\ .').hasMatch(name)) {
         List<String> names = name.split(' ');
-        List<String> firsts = names.sublist(0, names.length-1);
+        List<String> firsts = names.sublist(0, names.length - 1);
 
         return User(firsts.join(' '), names[names.length - 1], pb);
-      } else { 
+      } else {
         return User(name, '', pb);
       }
     } else {
       return User('Not', 'Registered', pb);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
-                top: 18,
-                left: 14,
-                right: 14,
-                height: 52,
-                child: FutureBuilder<User>(
-                    future: _getNames(),
-                    builder:
-                        (BuildContext context, AsyncSnapshot<User> snapshot) {
-                      User user = User("Maximilian", "Laue", Image.asset('assets/Personavatar.png'));
-                      if(snapshot.hasData) {
-                        user = snapshot.data!;
-                      }    
-                        
-                      List<Widget> children = [
-                        Image.asset('assets/Personavatar.png'),
-                        Container(
-                            margin: const EdgeInsets.only(left: 14),
-                            child: Text.rich(
-                              TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: user.prename +'\n',
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                      fontFamily: 'Ubuntu',
-                                      fontWeight: FontWeight.w700,
-                                      height: 0,
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text: user.lastname,
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                      fontFamily: 'Ubuntu',
-                                      fontWeight: FontWeight.w400,
-                                      height: 0,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ))
-                      ];
-                      return Row(children: children);
-                    }));
-  }
+        top: 18,
+        left: 14,
+        right: 14,
+        height: 52,
+        child: FutureBuilder<User>(
+            future: _getNames(),
+            builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+              User user = User(
+                  "Maximilian", "Laue", Image.asset('assets/Personavatar.png'));
+              if (snapshot.hasData) {
+                user = snapshot.data!;
+              }
 
+              List<Widget> children = [
+                Image.asset('assets/Personavatar.png'),
+                Container(
+                    margin: const EdgeInsets.only(left: 14),
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: user.prename + '\n',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w700,
+                              height: 0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: user.lastname,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+              ];
+              return Row(children: children);
+            }));
+  }
 }
