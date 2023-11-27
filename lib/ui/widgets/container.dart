92a9c6729d366672fb7efb527c9366849900a3bb
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2023-11-27 19:38:47 +0100

    Container Für alle... Mit Scroll Funktion

--- lib/ui/widgets/container.dart
+++ lib/ui/widgets/container.dart
@@ -10,28 +10,32 @@ class CustomContainer extends StatelessWidget {
       {super.key, required this.title, this.smallSize, required this.children});
 
   @override
-  Widget build(BuildContext context) {
-  //  Size screenSize = MediaQuery.of(context).size;
-    return Container(
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 43, 43, 43).withOpacity(0.90),
-          borderRadius: BorderRadius.circular(34.5),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(22),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 30),
-              height: 25,
-              child: Text(
-                title,
-                style: Styles.overlayTitle,
-                textAlign: TextAlign.left,
-              ),
-            ),
-            ...children
-          ]),
-        ));
+  Widget build(BuildContext context, ) {
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+          height: constraints.biggest.height,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 43, 43, 43).withOpacity(0.90),
+            borderRadius: BorderRadius.circular(34.5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(22),
+            child:
+                SingleChildScrollView(
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                            Container(
+                  margin: const EdgeInsets.only(bottom: 30),
+                  height: 25,
+                  child: Text(
+                    title,
+                    style: Styles.overlayTitle,
+                    textAlign: TextAlign.left,
+                  ),
+                            ),
+                            ...children
+                          ]),
+                ),
+          )),
+    );
   }
 }
