e023eba20c4fdd5f19994d6c2e74f3a623537aa3
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2023-12-06 20:18:09 +0100

    oke

--- lib/ui/widgets/dashboardWidget.dart
+++ lib/ui/widgets/dashboardWidget.dart
@@ -12,22 +12,27 @@ class DashboardWidget extends StatefulWidget {
 class _DashboardWidgetState extends State<DashboardWidget> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: widget.elevation,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(34.4),
-      ),
-      color: const Color(0xE51E1E1E),
-      child: Padding(
-        padding: EdgeInsets.only(top: 8, bottom: 8, left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              super.widget.title,
-              style: Styles.dashboardWidgetTitle,
-            ),
-          ],
+    return GestureDetector(
+      onLongPress: () {
+        setState(() {});
+      },
+      child: Card(
+        elevation: widget.elevation,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(34.4),
+        ),
+        color: const Color(0xE51E1E1E),
+        child: Padding(
+          padding: EdgeInsets.only(top: 8, bottom: 8, left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                super.widget.title,
+                style: Styles.dashboardWidgetTitle,
+              ),
+            ],
+          ),
         ),
       ),
     );
