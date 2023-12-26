0f9236f018cb20495e7cb94757819faf48a6857f
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2023-12-26 16:47:47 +0100

    Image and PDFPage hinzugefügt

--- lib/ui/widgets/ticketWidgets/ticketContainer.dart
+++ lib/ui/widgets/ticketWidgets/ticketContainer.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_praktikum/ui/views/ticket/ImageViewerPage.dart';
+import 'package:internet_praktikum/ui/views/ticket/PDFViewerPage.dart';
 import 'package:internet_praktikum/ui/widgets/bottom_sheet.dart';
 
 class TicketContainer extends StatefulWidget {
@@ -19,6 +23,9 @@ class TicketContainer extends StatefulWidget {
 
 class _TicketContainerState extends State<TicketContainer> {
   Image? image;
+  bool isImageLoading = false; // TODO: loading animation npoh einbauen
+  bool isPDF = false;
+  String? imageUrlNew;
 
   @override
   void initState() {
@@ -40,6 +47,10 @@ class _TicketContainerState extends State<TicketContainer> {
           .child('files/$tripId/${widget.title}')
           .list()
           .then((value) => value.items.first.fullPath);
+
+      imageUrlNew = imageUrl;
+
+      imageUrl.contains('.pdf') ? isPDF = true : isPDF = false;
 
       var getDownloadUrlLink =
           await FirebaseStorage.instance.ref(imageUrl).getDownloadURL();
@@ -91,8 +102,17 @@ class _TicketContainerState extends State<TicketContainer> {
                             child: image != null
                                 ? GestureDetector(
                                     onTap: () {
-                                      //TODO: hier muss das Bild dann Größer werden, heißt eine Page öffnet sich => Preview Page
-                                      Navigator.of(context).pop();
+                                      (isPDF == true
+                                          ? openPDF(
+                                              context,
+                                              File(image!.toString()),
+                                              widget
+                                                  .title) // Das funktioniert nicht
+                                          : openImage(
+                                              context,
+                                              image!,
+                                              widget
+                                                  .title)); // Das funktioniert
                                     },
                                     child: image,
                                   )
@@ -155,4 +175,12 @@ class _TicketContainerState extends State<TicketContainer> {
       ),
     );
   }
+
+  void openPDF(BuildContext context, File file, String title) =>
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => PDFViewerPage(file: file, title: title)));
+
+  void openImage(BuildContext context, Image image, String title) =>
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ImageViewerPage(image: image, title: title)));
 }
