a5442442e13a0473a8ec4516c3af89b59de162f8
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2023-12-28 02:56:41 +0100

    info page farben geändert + navigationbar in map verändert

--- lib/ui/views/profile/info_page.dart
+++ lib/ui/views/profile/info_page.dart
@@ -10,93 +10,107 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //backgroundColor: const Color.fromARGB(255, 49, 48, 78),
+        backgroundColor: const Color.fromARGB(255, 49, 48, 78),
         leading: IconButton(
           onPressed: () {
             context.go("/profile");
           },
           icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.white,
         ),
         centerTitle: true,
-        title: const Text("The Creators "),
+        title: const Text("The Creators", style: Styles.infoTitle),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3 -
-                  kToolbarHeight / 3,
-              child: const Row(
-                children: [
-                  CharakterContainer(
-                    name: "Mithusan Naguleswaran",
-                    description: "Co Founder, CEO",
-                    link:
-                        "https://www.linkedin.com/in/mithusan-naguleswaran-b046a1292/",
-                    image: "assets/character_pic/mithu.png",
-                    color: Colors.red,
-                  ),
-                  CharakterContainer(
-                    name: "Thai Binh Nguyen",
-                    description: "Co Founder, CEO",
-                    link: "https://pub.dev/packages/url_launcher",
-                    image: "assets/character_pic/thai.png",
-                    color: Colors.purple,
-                  ),
-                ],
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 49, 48, 78),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3 -
+                    kToolbarHeight / 3,
+                child: const Row(
+                  children: [
+                    CharakterContainer(
+                      name: "Mithusan Naguleswaran",
+                      description: "Co Founder, CEO",
+                      link:
+                          "https://www.linkedin.com/in/mithusan-naguleswaran-b046a1292/",
+                      image: "assets/character_pic/mithu.png",
+                      color: Colors.red,
+                    ),
+                    CharakterContainer(
+                      name: "Thai Binh Nguyen",
+                      description: "Co Founder, CEO",
+                      link: "https://pub.dev/packages/url_launcher",
+                      image: "assets/character_pic/thai.png",
+                      color: Colors.purple,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3 -
-                  kToolbarHeight / 3,
-              child: const Row(
-                children: [
-                  CharakterContainer(
-                    name: "Tim Carlo Päpke",
-                    description: "Co Founder, CEO",
-                    link: "bla bla bla",
-                    image: "assets/character_pic/mithu.png",
-                    color: Colors.orange,
-                  ),
-                  CharakterContainer(
-                    name: "Felix Bauer",
-                    description: "Co Founder, CEO",
-                    link: "bla bla bla",
-                    image: "assets/character_pic/mithu.png",
-                    color: Colors.green,
-                  ),
-                ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3 -
+                    kToolbarHeight / 3,
+                child: const Row(
+                  children: [
+                    CharakterContainer(
+                      name: "Tim Carlo Päpke",
+                      description: "Co Founder, CEO",
+                      link: "bla bla bla",
+                      image: "assets/character_pic/mithu.png",
+                      color: Colors.orange,
+                    ),
+                    CharakterContainer(
+                      name: "Felix Bauer",
+                      description: "Co Founder, CEO",
+                      link: "bla bla bla",
+                      image: "assets/character_pic/mithu.png",
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3 -
-                  kToolbarHeight / 3,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CharakterContainer(
-                    name: "David Henn",
-                    description: "Co Founder, CEO",
-                    link: "bla bla bla",
-                    image: "assets/character_pic/mithu.png",
-                    color: Colors.blue,
-                    fill: true,
-                  ),
-                ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3 -
+                    kToolbarHeight / 3,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CharakterContainer(
+                      name: "David Henn",
+                      description: "Co Founder, CEO",
+                      link: "bla bla bla",
+                      image: "assets/character_pic/mithu.png",
+                      color: Colors.blue,
+                      fill: true,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Text(
-                "Thank you for using our App, We hope you enjoy our App",
-                style: Styles.endCredits),
-            const Text("We are a group of 5 students of TU Darmstadt",
-                style: Styles.endCredits),
-            const Text(
-                "and created this App for our Internet Praktikum project",
-                style: Styles.endCredits),
-            const Text(
-                "Feel free to contact us for any suggestions or questions",
-                style: Styles.endCredits)
-          ],
+              const Divider(
+                color: Colors.white,
+                thickness: 2,
+              ),
+              const SizedBox(height: 10),
+              const Text("Credits",
+                  style: Styles.endCredits), // vlt was anderes dafür nehmen
+              const SizedBox(height: 10),
+              const Text(
+                  "Thank you for using our App, We hope you enjoy our App",
+                  style: Styles.endCredits),
+              const Text("We are a group of 5 students of TU Darmstadt",
+                  style: Styles.endCredits),
+              const Text(
+                  "and created this App for our Internet Praktikum project",
+                  style: Styles.endCredits),
+              const Text(
+                  "Feel free to contact us for any suggestions or questions",
+                  style: Styles.endCredits)
+            ],
+          ),
         ),
       ),
     );
