085ef136bdfa23816e5a051c340cba1177613b19
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2024-01-15 02:35:10 +0100

    weatherpage geändert

--- lib/ui/views/weather/weather_page.dart
+++ lib/ui/views/weather/weather_page.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:internet_praktikum/core/services/weather_service.dart';
 import 'package:internet_praktikum/ui/views/weather/weather.dart';
+import 'package:internet_praktikum/ui/widgets/my_button.dart';
 
 import 'package:lottie/lottie.dart';
 
+// ignore: must_be_immutable
 class WeatherPage extends StatefulWidget {
-  final Weather actualWeather;
+  Weather actualWeather;
+  final WeatherService _weatherHandler =
+      WeatherService("5a9d3eda46bcddc1662d351abc13c798");
 
-  const WeatherPage({
+  WeatherPage({
     super.key,
     required this.actualWeather,
   });
@@ -18,6 +22,9 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+
+  //TODO: oben rechts soll nicht die gleichen Buttons sein wie die unten sondern, lieber ein Dark-White Mode Button
+  //TODO Maybe getCity in WeatherService löschen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +40,21 @@ class _WeatherPageState extends State<WeatherPage> {
           actions: [
             IconButton(
               icon: const Icon(Icons.refresh, color: Colors.white, size: 30),
-              onPressed: () {
-                //fetchWeather();
+              onPressed: () async {
+                final weather = await widget._weatherHandler.fetchWeather();
+                setState(() {
+                  widget.actualWeather = weather!;
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.directions, color: Colors.white, size: 30),
+              onPressed: () async {
+                final weather =
+                    await widget._weatherHandler.fetchWeatherForCurrentCity();
+                setState(() {
+                  widget.actualWeather = weather;
+                });
               },
             ),
           ]),
@@ -58,6 +78,34 @@ class _WeatherPageState extends State<WeatherPage> {
             // weather condition
             Text(widget.actualWeather.mainCondition,
                 style: const TextStyle(fontSize: 30, color: Colors.white)),
+
+            const SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 20),
+              child: MyButton(
+                  onTap: () async {
+                    final weather = await widget._weatherHandler.fetchWeather();
+                    setState(() {
+                      widget.actualWeather = weather!;
+                    });
+                  },
+                  text: "Get Weather for your Trip Location"),
+            ),
+
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 20),
+              child: MyButton(
+                  onTap: () async {
+                    final weather = await widget._weatherHandler
+                        .fetchWeatherForCurrentCity();
+                    setState(() {
+                      widget.actualWeather = weather;
+                    });
+                  },
+                  text: "Get Weather for your Actual Location"),
+            ),
           ],
         ),
       ),
